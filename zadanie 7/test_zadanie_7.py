a26e03520f4e620773d28973734c2f70f114c213
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2021-05-03 21:41:07 +0200

    Update test_zadanie_7.py

--- zadanie 7/test_zadanie_7.py
+++ zadanie 7/test_zadanie_7.py
@@ -1,13 +1,12 @@
 import pytest
 
-
-
-  dzielenie = [
+dzielenie = [
     (15, 3, 5),
     (20, 5, 4)
- ]
+]
 
- @pytest.mark.parametrize("a,b,expected", dzielenie)
- def test_dzielenie_v0(a, b, expected):
-       diff = a / b
-       assert diff == expected
+
+@pytest.mark.parametrize("a,b,expected", dzielenie)
+def test_dzielenie_v0(a, b, expected):
+    diff = a / b
+    assert diff == expected
